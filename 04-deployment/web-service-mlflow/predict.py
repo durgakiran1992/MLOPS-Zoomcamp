b6f879af847124e1b4bdffecf7060d7552097078
durgakiran1992 <168992132+durgakiran1992@users.noreply.github.com>
2025-07-22 15:29:57 +0000

    Module-4

---
+++ 04-deployment/web-service-mlflow/predict.py
@@ -0,0 +1,48 @@
+import os
+import boto3
+import pickle
+from mlflow.tracking import MlflowClient
+import mlflow
+from flask import Flask, request, jsonify
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# RUN_ID = os.getenv('RUN_ID')
+RUN_ID = '5e1a8d1da960432b8a921ffdec3965f7'
+
+# logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
+logged_model = f'runs:/{RUN_ID}/model'
+model = mlflow.pyfunc.load_model(logged_model)
+
+
+def prepare_features(ride):
+    features = {}
+    features['PU_DO'] = '%s_%s' % (ride['PULocationID'], ride['DOLocationID'])
+    features['trip_distance'] = ride['trip_distance']
+    return features
+
+
+def predict(features):
+    preds = model.predict(features)
+    return float(preds[0])
+
+
+app = Flask('duration-prediction')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json()
+
+    features = prepare_features(ride)
+    pred = predict(features)
+
+    result = {
+        'duration': pred,
+        'model_version': RUN_ID
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
